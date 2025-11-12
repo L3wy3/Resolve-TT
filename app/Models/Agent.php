2ffcd080bd611505ed28f140bad384e65361013a
Laurence Towning <lstowning@outlook.com>
2025-11-12 16:28:43 +0000

    Initial commit and database seeding

---
+++ app/Models/Agent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agent extends Model
+{
+    /** @use HasFactory<\Database\Factories\AgentFactory> */
+    protected $fillable = ['name', 'target', 'current_sales'];
+    use HasFactory;
+}
---
+++ database/factories/AgentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Agent>
+ */
+class AgentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'target' => $this->faker->randomFloat(2,100,1000),
+            'current_sales' => $this->faker->randomFloat(2,100,1000),
+        ];
+    }
+}
